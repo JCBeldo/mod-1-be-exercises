9452c62da68e3573a515bc17db9a44c4de712314
Julian Beldotti <julian.beldotti@gmail.com>
2023-02-01 18:40:11 -0500

    Test turn

--- flash_cards/lib/turn.rb
+++ flash_cards/lib/turn.rb
@@ -2,15 +2,25 @@ class Turn
   def initialize(guess, card)
     @guess = guess
     @card = card
-    @correct = true
-  # end
-  # def guess
-  #   @guess
-  # end
-  # def feedback
-  #   if @correct == true
-  #     p "Correct!"
-  #   else "Incorrect."
-  #   end
+  end
+  def guess
+    @guess
+  end
+  def card
+    @card
+  end
+  def correct?
+    if @guess == 'Mercury'
+      true
+    else
+      false
+    end
+  end
+  def feedback
+    if correct? == true
+      p "Correct!"
+    else "Incorrect."
+    end
   end
 end
+
--- flash_cards/spec/turn_spec.rb
+++ flash_cards/spec/turn_spec.rb
@@ -8,24 +8,6 @@ RSpec.describe Turn do
 
     expect(turn).to be_instance_of(Turn)
   end
-
-#   it 'has a question' do
-#     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-
-#     expect(card.question).to eq("What is the capital of Alaska?")
-#   end
-
-#   it 'has an answer' do
-#     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-
-#     expect(card.answer).to eq("Juneau")
-#   end
-
-#   it 'has a category' do
-#     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-
-#     expect(card.category).to eq(:Geography)
-#   end
 end
 # pry(main)> card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 # #=> #<Card:0x00007f800e29f0c8 @question=""What is the capital of Alaska?", @answer="Juneau", @category=:Geography>
