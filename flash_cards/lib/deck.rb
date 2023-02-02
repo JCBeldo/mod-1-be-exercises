1c68caaaa359275c1837be130a4b4ef99873583e
Julian Beldotti <julian.beldotti@gmail.com>
2023-02-01 23:39:42 -0500

    Attempt deck

---
+++ flash_cards/lib/deck.rb
@@ -0,0 +1,10 @@
+class Deck
+  def initialize(cards)
+    @cards = []
+    @count = count
+    @
+  end
+  def cards
+    @cards = card_1, card_2, card_3
+  end
+end
---
+++ flash_cards/spec/deck_spec.rb
@@ -0,0 +1,8 @@
+require 'rspec'
+require './lib/card'
+require './lib/deck'
+
+RSpec.describe Deck do
+  describe '#initialize' do
+end
+end
