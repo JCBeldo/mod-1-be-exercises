f733395a0d812f8e6254a0be65da0fee92d6b80e
Julian Beldotti <julian.beldotti@gmail.com>
2023-02-02 22:49:07 -0500

    Update deck class

--- flash_cards/lib/deck.rb
+++ flash_cards/lib/deck.rb
@@ -1,10 +1,23 @@
 class Deck
+  attr_reader :cards
+  
   def initialize(cards)
-    @cards = []
-    @count = count
-    @
+    # @cards = [card_1, card_2, card_3]
+    @cards = cards
   end
-  def cards
-    @cards = card_1, card_2, card_3
+  # def cards
+  #   @cards = [] << @@cards
+  # end
+  def count
+    @cards.count
+  end
+  def cards_in_category(category)
+    category_cards = []
+    @cards.each do |card|
+      if card.category == category
+        category_cards << card
+      end
+    end
+
   end
 end
--- flash_cards/spec/deck_spec.rb
+++ flash_cards/spec/deck_spec.rb
@@ -4,5 +4,22 @@ require './lib/deck'
 
 RSpec.describe Deck do
   describe '#initialize' do
+
+  it 'exists' do
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+
+    expect(card_1).to be_a(Card)
+  end
+
+  it 'exists' do
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+
+    expect(card_2).to be_a(Card)
+  end
+
+  it 'exists' do
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    expect(card_3).to be_a(Card)
+  end
 end
 end
