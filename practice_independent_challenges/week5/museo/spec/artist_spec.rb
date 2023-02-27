8c1d7a43a582f56a330bbc8def211e1d6e3e0d79
Julian Beldotti <julian.beldotti@gmail.com>
2023-02-27 10:28:57 -0500

    Adds an artist spec and class

--- practice_independent_challenges/week5/museo/spec/artist_spec.rb
+++ practice_independent_challenges/week5/museo/spec/artist_spec.rb
@@ -2,8 +2,35 @@ require 'rspec'
 require 'spec_helper'
 
 RSpec.describe Artist do
-  describe '' do
-    it '' do
+  describe '#initialize' do
+    it 'exists and has attributes' do
+      artist = Artist.new({
+        id: "2",
+        name: "Ansel Adams",
+        born: "1902",
+        died: "1984",
+        country: "United States"
+      })
+
+      expect(artist).to be_a(Artist)
+      expect(artist.id).to eq("2")
+      expect(artist.name).to eq("Ansel Adams")
+      expect(artist.born).to eq("1902")
+      expect(artist.died).to eq("1984")
+      expect(artist.country).to eq("United States")
+    end
+  end
+  describe '#age at death' do
+    it 'shows age of artist at death' do
+      artist = Artist.new({
+        id: "2",
+        name: "Ansel Adams",
+        born: "1902",
+        died: "1984",
+        country: "United States"
+      })
+
+      expect(artist.age_at_death).to eq(82)
     end
   end
 end
